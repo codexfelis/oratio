e845a57b09a51decd96792e107634c493978104b
Caro <caro401@users.noreply.github.com>
2024-02-25 18:36:27 +0000

    Support authors (#17)
    * Set up formatter
    * Display author name(s) below page title
    * Add dan's website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://oratio.codexfelis.dev',
-  integrations: [starlight({
-    title: 'Oratio',
-    description: 'Tutorials and tech explainers for digital humanists, from Codex Felis',
-    favicon: '/favicon-32x32.png',
-	customCss: [
-        // Path to your Tailwind base styles:
-        './src/styles.css',
-		'@fontsource/eagle-lake/400.css',
-		'@fontsource-variable/work-sans'
-		
+  site: "https://oratio.codexfelis.dev",
+  integrations: [
+    starlight({
+      title: "Oratio",
+      description:
+        "Tutorials and tech explainers for digital humanists, from Codex Felis",
+      favicon: "/favicon-32x32.png",
+      customCss: [
+        "./src/styles.css",
+        "@fontsource/eagle-lake/400.css",
+        "@fontsource-variable/work-sans",
       ],
-    social: {
-      github: 'https://github.com/codexfelis/oratio'
-    },
-    sidebar: [{
-      label: 'Welcome!',
-	  autogenerate: {
-        directory: 'guides'
-      }
-
-    }, {
-      label: 'What is...',
-      autogenerate: {
-        directory: 'what-is'
-      }
-    }]
-  }),    ]
-  
+      social: {
+        github: "https://github.com/codexfelis/oratio",
+      },
+      components: {
+        PageTitle: "./src/components/PageTitle.astro",
+      },
+      sidebar: [
+        {
+          label: "Welcome!",
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "What is...",
+          autogenerate: {
+            directory: "what-is",
+          },
+        },
+      ],
+    }),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,23 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { z, reference, defineCollection } from "astro:content";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
+
+const authors = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    link: z.string().url().optional(),
+  }),
+});
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: () => {
+        return z.object({
+          authors: z.array(reference("authors")).optional(),
+        });
+      },
+    }),
+  }),
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
